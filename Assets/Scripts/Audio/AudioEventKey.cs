6d6b8d12b4ca45ab6db3f9efb12637d4920b5cd8
Can Uzer <mcanuzer@gmail.com>
2019-06-29 17:58:18 +0300

    Apply audio event updates to the new audio branch

--- Assets/Scripts/Audio/AudioEventKey.cs
+++ Assets/Scripts/Audio/AudioEventKey.cs
@@ -1,8 +1,65 @@
 public enum AudioEventKey
 {
     None = 0,
-    
-    CardReveal         = 1,
-    CardPick           = 2,
-    CardDrop           = 3
+
+    AmbienceGame = 1000,
+    AmbienceMenu = 1001,
+
+    CardAvatarDeath = 2000,
+    CardAvatarLootCollect = 2001,
+
+    CardBoardAgent = 2100,
+    CardBoardContainer = 2101,
+    CardBoardMonster = 2102,
+
+    CardClashBribe = 2201,
+    CardClashDestroy = 2202,
+    CardClashFight = 2203,
+    CardConfrontMarshal = 2204,
+    CardConfrontMonster = 2205,
+    CardConfrontPirate = 2206,
+
+    CardDefeatMarshal = 2300,
+    CardDefeatMonster = 2301,
+    CardDefeatPirate = 2302,
+
+    CardReveal = 2400,
+    CardRevealContainer = 2401,
+    CardRevealMonster = 2402,
+    CardRevealShip = 2403,
+
+    CardItemBribe = 2500,
+    CardItemStash = 2501,
+    CardItemTradeBuy = 2502,
+    CardItemTradeSell = 2503,
+
+    CardSupplyCascade = 2600,
+    CardSupplyRedeal = 2601,
+    CardSupplySinkShip = 2602,
+
+    CardToolHealthArm = 2700,
+    CardToolHealthUse = 2701,
+    CardToolMeleeArm = 2702,
+    CardToolMeleeUse = 2703,
+    CardToolRangedArm = 2704,
+    CardToolRangedHitCannon = 2705,
+    CardToolRangedHitCatapult = 2706,
+    CardToolRangedUseCannon = 2707,
+    CardToolRangedUseCatapult = 2708,
+    CardToolStash = 2709,
+
+    CardBridgeDismiss = 2800,
+
+    GameAssemble = 3000,
+    GameLose = 3001,
+    GameStart = 3002,
+    GameWin = 3003,
+
+    MusicGameLoop = 4000,
+
+    UIDragCancel = 5000,
+    UIDragGrab = 5001,
+    UITapButtonCancel = 5100,
+    UITapButtonConfirm = 5101,
+    UITapInvalid = 5102,
 }
